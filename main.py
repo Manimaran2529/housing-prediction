d78799ba33faa3a53f1a060f8c9cecc3969b5ab7
manimaran <manimaranh9965@gmail.com>
2026-01-24 19:23:18 +0530

    complete

--- main.py
+++ main.py
@@ -1,49 +1,80 @@
-# step1: read a file
-
-import pandas as pd #  its a library for read a file 
-from sklearn.preprocessing import StandardScaler # us for convert max number to mean
+import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
-mm=pd.read_csv("Housing.csv")# used for read a csv file
-list1=["bathrooms","stories"]# tthere is null value in the two columns soo we create a list
-mm[list1]=mm[list1].fillna(1)# we fill 1 forr the misiing values
-list2=["airconditioning"]# in this columns also there is null valeus in the string types 
-mm[list2]=mm[list2].fillna("yes") # we add a yes to the null valus 
-# print(mm[mm.duplicated()])# this is used for check the duplicate values and rows
-# mani=mm.drop_duplicates() # this is used for remove the duplicates 
-
-#step2:change string to numbers  by using ordinary encoder,one heart encoder,nlp,replace functions
-list3=["bathrooms","stories","prefarea","hotwaterheating","guestroom", "basement","mainroad","airconditioning"]#we create a list for convert 0 ans 1
-mm[list3]=mm[list3].replace({"yes":0,"no":1})# this is used for convert 1 and 0 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PowerTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+mm=pd.read_csv("Housing.csv")
 
 
-#step3  we used a one heart encoder for convert furnishingstatus into decimal why we  use onehaert encoder iths has more than 2 columns
+list1=["bathrooms","stories",]
+mm[list1]=mm[list1].fillna(2)
 
-#mm=pd.get_dummies(mm, columns=["furnishingstatus"] )
-mm = pd.get_dummies(mm, columns=["furnishingstatus"],dtype=int)# its onehot encoder to convert into 0 and 1s
+mm["airconditioning"]=mm["airconditioning"].fillna("yes")
+
+list2=["mainroad","guestroom","basement","hotwaterheating","airconditioning","prefarea"]
+mm[list2] = mm[list2].replace({"yes": 1, "no": 0})
+print(mm["furnishingstatus"].unique())
+mm=pd.get_dummies(mm,columns=["furnishingstatus"],dtype=int)
 
 
-#step4  i the datasets there is long number so  he model accuracy is misiing so convert  numbers into  mean by using a normalization and standscalar 
-list4=["price","area"]# this a datasets we want to change
-mm[list4]=StandardScaler().fit_transform(mm[list4])# we use a standarscaler for change 
-#print(mm.head(10)) # round is used for print the laste 2 decimnals only
 
-#corr=mm.corr()
+#cor=mm.corr()
 #plt.figure(figsize=(10,10))
-#sns.heatmap(
-  #  corr,annot=True,cmap="coolwarm",
+#sns.heatmap(cor,annot=True,cmap="coolwarm")
+#plt.title("corelation map")
+#plt.show()
 
-#)
-##plt.title("relation map")
 
-X = mm[["area","bathrooms","stories","parking","bedrooms"]]
-y = mm["price"]
-sns.scatterplot(
-    data=mm,
-    x="area",
-    y="price",
-    hue="mainroad"
-)
-plt.title("Price vs Area by Mainroad")
-plt.show()
- 
+x=mm[["area","bedrooms","bathrooms","stories","airconditioning","parking","prefarea","mainroad"]]
+y=mm["price"]
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=10)
+
+
+
+pd_x=PowerTransformer(method="yeo-johnson")
+x_train_pd=pd_x.fit_transform(x_train)
+x_test_pd=pd_x.transform(x_test)
+
+pd_y=PowerTransformer(method="yeo-johnson")
+y_train_resize=y_train.values.reshape(-1,1)
+y_test_resize=y_test.values.reshape(-1,1)
+
+y_train_pd=pd_y.fit_transform(y_train_resize)
+y_test_pd=pd_y.transform(y_test_resize)
+
+
+lr=LinearRegression()
+lr.fit(x_train_pd,y_train_pd)
+
+y_pre=lr.predict(x_test_pd)
+
+print("r2score",r2_score(y_test_pd,y_pre))
+area = float(input("Enter area (sqft): "))
+bedrooms = int(input("Enter number of bedrooms: "))
+bathrooms = int(input("Enter number of bathrooms: "))
+stories = int(input("Enter number of stories: "))
+airconditioning = int(input("Do you want AC? yes=1, no=0: "))
+parking = int(input("Enter parking (0/1/2): "))
+prefarea = int(input("Preferred area? yes=1, no=0: "))
+mainroad = int(input("Near main road? yes=1, no=0: "))
+mani = [[
+    area,
+    bedrooms,
+    bathrooms,
+    stories,
+    airconditioning,
+    parking,
+    prefarea,
+    mainroad
+]]
+
+pd_mani=pd_x.transform(mani)
+
+pd_predict=lr.predict(pd_mani)
+
+maran=pd_y.inverse_transform(pd_predict)
+
+print("house price",maran[0][0])
--- pratice.py
+++ pratice.py
@@ -1,12 +1,13 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler,PowerTransformer
+from sklearn.preprocessing import PowerTransformer
 from sklearn.model_selection import train_test_split
-
 from matplotlib  import pyplot as plt
 import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
+
+
 mm=pd.read_csv("Housing.csv")
-
-
 list1=["bathrooms","stories"]
 mm[list1]=mm[list1].fillna(2)
 
@@ -15,7 +16,8 @@ mm["airconditioning"]=mm["airconditioning"].fillna("yes")
 
 
 list2=["prefarea","hotwaterheating","guestroom", "basement","mainroad","airconditioning"]#we create a list for convert 0 ans 1
-mm[list2]=mm[list2].replace({"yes":1,"no":0})
+mm[list2] = mm[list2].replace({"yes": 1, "no": 0}).infer_objects(copy=False)
+
 
 
 mm=pd.get_dummies(mm ,columns=["furnishingstatus"],dtype=int)
@@ -30,24 +32,27 @@ x=mm[["area","bedrooms","bathrooms","stories","mainroad","airconditioning","park
 y=mm["price"]
 
 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=45)
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=10)
 
-pt=PowerTransformer(method="yeo-johnson")
-X_train=pt.fit_transform(x_train)
-X_test=pt.transform(x_test)
+pt_x=PowerTransformer(method="yeo-johnson")
+X_train=pt_x.fit_transform(x_train)
+X_test=pt_x.transform(x_test)
 
 
 
-pt_x = PowerTransformer(method="yeo-johnson")
+pt_y = PowerTransformer(method="yeo-johnson")
 
 Y_train_resize=y_train.values.reshape(-1,1)
 Y_test_resize=y_test.values.reshape(-1,1)
 
-Y_train_pt=pt.fit_transform(Y_train_resize)
-Y_test_pt=pt.transform(Y_test_resize)
 
-sns.histplot(Y_train_pt)
-plt.title("after PowerTransformer")
-plt.show()
+Y_train_pt=pt_y.fit_transform(Y_train_resize)
+Y_test_pt=pt_y.transform(Y_test_resize)
 
+clf=LinearRegression()
+clf.fit(X_train,Y_train_pt)
+
+Y_pred=clf.predict(X_test)
+
+print("r2score:",r2_score(Y_test_pt,Y_pred))
 
